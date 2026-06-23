034518fc5aa7add34669bf5e0a591efaf26aa306
RGH <hugo@gmail.com>
2026-06-23 19:07:11 +0200

    Task 7 Completed: Implement ConcertView and Main correctly.

--- src/main/java/edu/iesam/Main.java
+++ src/main/java/edu/iesam/Main.java
@@ -1,10 +1,20 @@
 package edu.iesam;
 
+import edu.iesam.features.concerts.presentation.ConcertView;
+
 public class Main {
 
     static void main() {
 
+        //Function1
+        System.out.println("Actual list");
+        ConcertView.printConcerts();
 
+        //Function2
+        ConcertView.saveConcert();
+
+        //Function3
+        ConcertView.deleteConcert();
 
     }
 
---
+++ src/main/java/edu/iesam/features/concerts/presentation/ConcertView.java
@@ -0,0 +1,56 @@
+package edu.iesam.features.concerts.presentation;
+
+import edu.iesam.features.concerts.data.ConcertDataRepository;
+import edu.iesam.features.concerts.data.ConcertMemLocalDataSource;
+import edu.iesam.features.concerts.domain.Concert;
+import edu.iesam.features.concerts.domain.DeleteConcertUseCase;
+import edu.iesam.features.concerts.domain.GetConcertsUseCase;
+import edu.iesam.features.concerts.domain.SaveConcertUseCase;
+
+import java.util.ArrayList;
+
+public class ConcertView {
+
+    //Funcion 1
+    public static void printConcerts() {
+        GetConcertsUseCase getConcertsUseCase = new GetConcertsUseCase(
+                new ConcertDataRepository(ConcertMemLocalDataSource.newInstance())
+        );
+
+        ArrayList<Concert> concerts = getConcertsUseCase.execute();
+        System.out.println(concerts);
+    }
+
+    //Funcion 2
+    public static void saveConcert() {
+        SaveConcertUseCase saveConcertUseCase = new SaveConcertUseCase(
+                new ConcertDataRepository(ConcertMemLocalDataSource.newInstance())
+        );
+
+        System.out.println("List before");
+        printConcerts();
+
+        Concert newConcert = new Concert("4","Gabriel","Nº4","23/06/2026","2000","206");
+        System.out.println("Saving: " + newConcert);
+        saveConcertUseCase.execute(newConcert);
+
+        System.out.println("List after");
+        printConcerts();
+    }
+
+    //Funcion 3
+    public static void deleteConcert() {
+        DeleteConcertUseCase deleteConcertUseCase = new DeleteConcertUseCase(
+                new ConcertDataRepository(ConcertMemLocalDataSource.newInstance())
+        );
+
+        System.out.println("List before");
+        printConcerts();
+
+        System.out.println("Deleting... ");
+        deleteConcertUseCase.execute("2");
+
+        System.out.println("List after");
+        printConcerts();
+    }
+}
